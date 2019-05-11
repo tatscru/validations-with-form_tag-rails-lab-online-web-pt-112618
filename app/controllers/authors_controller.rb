52637729e6095ead4c993782fb94b2870b12a7d6
tatiana.mojica <tatiana.mojica@bustle.com>
2019-05-11 17:00:49 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,8 +9,12 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   def edit
